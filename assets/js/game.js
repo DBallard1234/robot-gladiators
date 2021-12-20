6af19be9ade1356b8a9fa442522105c6b3fed840
David Ballard <ballard.david97@icloud.com>
2021-12-19 20:32:20 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,6 +68,12 @@ var fight = function(enemyName) {
     }
   }
 };
+// function to start a new game
+var startGame = function () { 
+    // reset player stats
+    playerHealth = 100; 
+    playerAttack = 10; 
+    playerMoney = 10;
 
 // fight each enemy-robot by looping over them and fighting them one at a time
 for (var i = 0; i < enemyNames.length; i++) {
@@ -93,5 +99,32 @@ for (var i = 0; i < enemyNames.length; i++) {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
   }
+
+  //after the loop ends, player is either out of health or enemies to fight, so run the endGame Function 
+  endGame();
+}; 
+// play again
+startGame(); 
+//funtion to end the entire game
+var endGame = function() { 
+    // if player is still alive, player wins!
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a  scoe or " + playerMoney +".");
+    } 
+    else {
+        window.alert("You've lost your robot in battle");
+    }
+} 
+//ask player if they'd like to play again 
+var playAgainConfirm = window.confirm("Would you like to play again?")
+
+if (playAgainConfirm) {
+    //restart the game
+    startGame(); 
 }
+else {
+    window.alert("Thank you for playing Robot Gladiators! Come Back Soon!");
+}
+};  
+
   
